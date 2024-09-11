75f26d79efb1276fe104b0820e6baf244ff780a0
s <s@MSI>
2024-09-11 11:33:29 -0400

    Add project files.

---
+++ MonitorDrive/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MonitorDrive
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
